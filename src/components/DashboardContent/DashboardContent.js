d7b1d4e99a85a0991b57b5d4af65de9e844df4ce
Yves Findlay <YvesFindlay@Yvess-MBP.lan>
2021-10-20 18:00:36 +0100

    Added initial DashboardContent component

---
+++ src/components/DashboardContent/DashboardContent.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+import "./DashboardContent.scss";
+
+const DashboardContent = () => {
+  return <div className="content"></div>;
+};
+
+export default DashboardContent;
