51860333ee699916bd4dfb01a59715246478e481
Yves Findlay <YvesFindlay@Yvess-MBP.lan>
2021-10-22 19:02:05 +0100

    Modified menu items and styling

--- src/components/Menu/Menu.js
+++ src/components/Menu/Menu.js
@@ -6,14 +6,13 @@ import "./Menu.scss";
 
 const Menu = () => {
   return (
-    // must add is active class
     <div className="menu">
       <ul>
         <MenuItem name="Account" icon="las las la-user" />
         <MenuItem name="Dashboard" icon="las la-adjust" active={true} />
-        <MenuItem name="Videos" icon="las la-video" />
+        <MenuItem name="Services" icon="la la-exchange-alt" />
         <MenuItem name="Analytics" icon="las la-chart-bar" />
-        <MenuItem name="Schedule" icon="las la-calendar" />
+        <MenuItem name="Settings" icon="las la-tools" />
       </ul>
     </div>
   );
